88ea486ffba2363f6774265e83928ad9e96bfad1
Manh9 <112113077+Manh9@users.noreply.github.com>
2023-03-03 09:38:09 +0700

    Add files via upload

---
+++ Bài thi java/bài thi 1/Exercise1/TestBank.java
@@ -0,0 +1,9 @@
+package Exercise1;
+
+public class TestBank {
+    public static void main(String[] args) {
+        Bank myBank = new Bank(1000, 10);
+        double monthlyInterest = myBank.calculateInterest();
+        System.out.println("Monthly interest: $" + monthlyInterest);
+    }
+}
