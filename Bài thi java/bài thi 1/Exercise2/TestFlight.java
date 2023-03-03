dc575ecde1549665dcba6c0a881852d25e973389
Manh9 <112113077+Manh9@users.noreply.github.com>
2023-03-03 09:38:54 +0700

    Update TestFlight.java

--- Bài thi java/bài thi 1/Exercise2/TestFlight.java
+++ Bài thi java/bài thi 1/Exercise2/TestFlight.java
@@ -3,6 +3,8 @@ package Exercise2;
 public class TestFlight {
         public static void main(String[] args) {
             Flight g1 = new Flight(857, "Toronto");
+            System.out.println(g1.getNumber());
+            System.out.println(g1.getDestination());
             g1.display();
         }
     }
